88184a9760123c6e1dff416a39e279f7648eb453
Natnael kebede <71388535+natikebede@users.noreply.github.com>
2024-04-21 17:04:05 +0300

    updated the server

--- src/Apis/api.js
+++ src/Apis/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
 
-    baseURL:'https://g-m-s-server.cubeaddis.com',
+    baseURL:'https://staging-g-m-s-server.cubeaddis.com',
     // baseURL:'http://127.0.0.1:5004',
 });
